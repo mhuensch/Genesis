66d9b104a926044e6e5ece4eb772139ef63c9806
Michael Huensch <mrh@run00.com>
2013-05-25 00:00:32 -0500

    Adding DefaultActionTypeAttribute

---
+++ Run00.Genesis/DefaultActionTypeAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Run00.Genesis
+{
+	public class DefaultActionTypeAttribute : Attribute
+	{
+		public Type ActionType { get; private set; }
+		public Type ModelType { get; private set; }
+
+		public DefaultActionTypeAttribute(Type actionType, Type modelType)
+		{
+			ActionType = actionType;
+			ModelType = modelType;
+		}
+	}
+}
