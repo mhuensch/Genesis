155ad7e2c1045c38cc24ecfa352c1f02ecde3fb6
Michael Huensch <support@run00.com>
2013-09-04 17:47:02 -0500

    Starting new contracts refactor

---
+++ Run00.Genesis/CreatorOptions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Run00.Genesis
+{
+	public class CreatorOptions
+	{
+		public int RecursionDepth { get; set; }
+		public int ListCount { get; set; }
+	}
+}
---
+++ Run00.Genesis/ICreator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Run00.Genesis
+{
+	public interface ICreator
+	{
+		T Create<T>(CreatorOptions options);
+		object Create(Type type, CreatorOptions options);
+	}
+}
