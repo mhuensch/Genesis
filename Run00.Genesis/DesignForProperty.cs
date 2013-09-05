f8b6817293c80dbba4a7b03e76c63df2e20037ac
Michael Huensch <mrh@run00.com>
2013-09-05 08:09:30 -0500

    Fixing build with missing file

---
+++ Run00.Genesis/DesignForProperty.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Run00.Genesis
+{
+	public class DesignForProperty
+	{
+		public PropertyInfo ForProperty { get; set; }
+		public Func<object> SetUsing { get; set; }
+	}
+}
